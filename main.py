20cb3a4694fd91cc76251bc577be098b7b2be627
Mateusz Wisniewski <83494@student.ue.poznan.pl>
2024-10-13 21:52:36 +0200

    Proba 1

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import string
+from collections import defaultdict
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+def wyczysc_tekst(tekst):
+    translator = str.maketrans('', '', string.punctuation)
+    return tekst.translate(translator).lower().split()
+
+liczba_dokumentow = int(input("Ile dokumentów chcesz wprowadzić? "))
+
+dokumenty = []
+
+for i in range(1, liczba_dokumentow + 1):
+    dokument = input(f"Wpisz treść dokumentu {i}: ").strip()
+    dokumenty.append(dokument)
+
+liczba_slow = int(input("Ile zapytań chcesz wprowadzić? "))
+
+
+slowa = []
+
+for i in range(1, liczba_slow + 1):
+    slowo = input(f"Wpisz zapytanie {i}: ").strip().lower()  # Zamiana na małe litery
+    slowa.append(slowo)
+
+wyniki = defaultdict(lambda: defaultdict(int))
+
+for i, dokument in enumerate(dokumenty):
+    dokument_id = i  # Indeks dokumentu
+
+    oczyszczony_dokument = wyczysc_tekst(dokument)
+
+    for slowo in slowa:
+        liczba_wystapien = oczyszczony_dokument.count(slowo)  # Zliczanie słowa w oczyszczonym dokumencie
+        wyniki[dokument_id][slowo] = liczba_wystapien
+
+for slowo in slowa:
+    dokumenty_wyniki = [(doc_id, wyniki[doc_id][slowo]) for doc_id in range(liczba_dokumentow) if
+                        wyniki[doc_id][slowo] > 0]
+
+    dokumenty_wyniki.sort(key=lambda x: (-x[1], x[0]))
+
+    wynik = [doc_id for doc_id, _ in dokumenty_wyniki]
+    print(wynik)
+
